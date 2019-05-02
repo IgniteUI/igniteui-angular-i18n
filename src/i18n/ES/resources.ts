087fb9f94d4ea7ee613cff3a4e77ca6a963bac8d
Excali2 <50200716+Excali2@users.noreply.github.com>
2019-05-02 12:52:53 +0200

    Created resources.ts file for Spanish
    Created resources.ts file for Spanish resource strings

---
+++ src/i18n/ES/resources.ts
@@ -0,0 +1,11 @@
+    
+import { GridResourceStringsES } from './grid-resources';
+import { TimePickerResourceStringsES } from './time-picker-resources';
+
+/**
+ * Spanish resource strings for all components
+ */
+export const IgxResourceStringsES = {
+    ...GridResourceStringsES,
+    ...TimePickerResourceStringsES
+};
