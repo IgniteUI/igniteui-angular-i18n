696ee761e8d9b32feb3fe3a92813cfd8b5d48140
Nadia Robakova <nrobakova@infragistics.com>
2019-11-11 15:09:20 +0200

    chore(*): add localization for carousel

---
+++ src/i18n/ES/carousel-resources.ts
@@ -0,0 +1,5 @@
+import { ICarouselResourceStrings } from 'igniteui-angular';
+
+export const CarouselResourceStringsES: ICarouselResourceStrings = {
+    igx_carousel_of: 'of'
+};
---
+++ src/i18n/JA/carousel-resources.ts
@@ -0,0 +1,5 @@
+import { ICarouselResourceStrings } from 'igniteui-angular';
+
+export const CarouselResourceStringsJA: ICarouselResourceStrings = {
+    igx_carousel_of: 'of'
+};
