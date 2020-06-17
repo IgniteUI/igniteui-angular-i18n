e6c2f45c99e41c4d00c2ccc74a94deb14415c08b
IBarakov <ibarakov@icloud.com>
2020-06-17 11:50:28 +0300

    feat(list): add list localization

---
+++ src/i18n/KO/list-resources.ts
@@ -0,0 +1,6 @@
+import { IListResourceStrings } from 'igniteui-angular';
+
+export const ListResourceStringsKO: IListResourceStrings = {
+    igx_list_no_items: 'There are no items in the list.',
+    igx_list_loading: 'Loading data from the server...'
+};
