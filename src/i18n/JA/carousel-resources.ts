6a232bae49ba5d33889f9bc427cf870fce54a1a0
Damyan Petev <damyan.petev@gmail.com>
2020-08-04 15:51:12 +0300

    refactor: type assert exports as expanded resource interfaces, test run

--- src/i18n/JA/carousel-resources.ts
+++ src/i18n/JA/carousel-resources.ts
@@ -1,5 +1,7 @@
 import { ICarouselResourceStrings } from 'igniteui-angular';
 
-export const CarouselResourceStringsJA: ICarouselResourceStrings = {
+export const CarouselResourceStringsJA = {
     igx_carousel_of: '/'
+} as {
+    [Key in keyof ICarouselResourceStrings]: ICarouselResourceStrings[Key]; // Mapped expands when asserted w/ `as`
 };
