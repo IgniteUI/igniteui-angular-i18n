7b877919ef52614900f75acb44e7ccc7fd97ac94
IvayloG <inj.iganchev@gmail.com>
2020-08-05 15:40:19 +0300

    chore: English description for missing strings #37

--- src/i18n/KO/paginator-resources.ts
+++ src/i18n/KO/paginator-resources.ts
@@ -4,10 +4,10 @@ import { IPaginatorResourceStrings } from 'igniteui-angular';
 const PaginatorResourceStringsKO_: ExpandRequire<IPaginatorResourceStrings> = {
     igx_paginator_label: 'Items per page',
     igx_paginator_pager_text: 'of',
-    igx_paginator_first_page_button_text: 'TODO',
-    igx_paginator_previous_page_button_text: 'TODO',
-    igx_paginator_last_page_button_text: 'TODO',
-    igx_paginator_next_page_button_text: 'TODO'
+    igx_paginator_first_page_button_text: 'Go to first page',
+    igx_paginator_previous_page_button_text: 'Previous page',
+    igx_paginator_last_page_button_text: 'Go to last page',
+    igx_paginator_next_page_button_text: 'Next page'
 };
 
 /**
