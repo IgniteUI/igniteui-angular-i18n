b618099a7c1e258864e23e6befa0e6ad5069846e
Rumyana Andriova <54146583+randriova@users.noreply.github.com>
2020-08-11 08:26:13 +0300

    Adding localization for JP.

--- src/i18n/JA/grid-resources.ts
+++ src/i18n/JA/grid-resources.ts
@@ -93,7 +93,7 @@ const GridResourceStringsJA_: ExpandRequire<IGridResourceStrings> = {
     igx_grid_advanced_filter_initial_text: '"And" あるいは "Or" にリンクされた条件のグループの作成から始めます。',
     igx_grid_advanced_filter_column_placeholder: '列の選択',
     igx_grid_advanced_filter_value_placeholder: '値',
-    igx_grid_pinned_row_indicator: '固定'
+    igx_grid_pinned_row_indicator: '固定済み'
 };
 
 /**
