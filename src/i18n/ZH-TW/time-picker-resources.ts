a6aab1997818a3d65b2afe8529bca000adcfa17d
iganchev <iganchev@infragistics.com>
2021-01-18 17:08:16 +0200

    Add structure for several new languages support
     #44

---
+++ src/i18n/ZH-TW/time-picker-resources.ts
@@ -0,0 +1,13 @@
+import { ITimePickerResourceStrings } from 'igniteui-angular';
+
+// exported below as re-cast to create declaration type with expanded properties
+const TimePickerResourceStringsZHTW_: ExpandRequire<ITimePickerResourceStrings> = {
+    igx_time_picker_ok: 'OK',
+    igx_time_picker_cancel: 'Cancel'
+};
+
+
+/**
+ * Chinese (Taiwan) zh-tw resource strings for IgxTimePicker
+ */
+export const TimePickerResourceStringsZHTW = TimePickerResourceStringsZHTW_ as ExpandRequire<ITimePickerResourceStrings>;
