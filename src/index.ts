02abaa9026ae2487f4a63d53b16c2d9490932c54
MPopov <desig9stein@gmail.com>
2021-01-21 14:52:20 +0200

    Adding Calendar RS for localization

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+// Japanese
+export * from './i18n/JA/calendar-resources';
 export * from './i18n/JA/grid-resources';
 export * from './i18n/JA/paginator-resources';
 export * from './i18n/JA/time-picker-resources';
@@ -5,6 +7,8 @@ export * from './i18n/JA/date-range-picker-resources';
 export * from './i18n/JA/carousel-resources';
 export * from './i18n/JA/list-resources';
 export * from './i18n/JA/resources';
+// Korean
+export * from './i18n/KO/calendar-resources';
 export * from './i18n/KO/grid-resources';
 export * from './i18n/KO/paginator-resources';
 export * from './i18n/KO/time-picker-resources';
@@ -12,6 +16,8 @@ export * from './i18n/KO/date-range-picker-resources';
 export * from './i18n/KO/carousel-resources';
 export * from './i18n/KO/list-resources';
 export * from './i18n/KO/resources';
+// Spanish
+export * from './i18n/ES/calendar-resources';
 export * from './i18n/ES/grid-resources';
 export * from './i18n/ES/paginator-resources';
 export * from './i18n/ES/time-picker-resources';
