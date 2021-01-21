34fbae10b85ccda23eea385fab8b79a5d27925aa
Daniel Rose <danrose@gmx.de>
2021-01-21 10:31:27 +0100

    Use default file layout

--- src/i18n/DE/calendar-resources.ts
+++ src/i18n/DE/calendar-resources.ts
@@ -1,21 +1,7 @@
-export interface ICalendarResourceStrings {
-    igx_calendar_previous_month: string;
-    igx_calendar_next_month: string;
-    igx_calendar_select_month: string;
-    igx_calendar_select_year: string;
-    igx_calendar_range_start: string;
-    igx_calendar_range_end: string;
-    igx_calendar_selected_month_is: string;
-    igx_calendar_first_picker_of: string;
-    igx_calendar_multi_selection: string;
-    igx_calendar_range_selection: string;
-    igx_calendar_single_selection: string;
-    igx_calendar_singular_multi_selection: string;
-    igx_calendar_singular_range_selection: string;
-    igx_calendar_singular_single_selection: string;
-   }
-   
-   export const CalendarResourceStringsDE: ICalendarResourceStrings = {
+import { ICalendarResourceStrings } from 'igniteui-angular';
+
+// exported below as re-cast to create declaration type with expanded properties
+const CalendarResourceStringsDE_: ExpandRequire<ICalendarResourceStrings> = {
     igx_calendar_previous_month: 'Vorheriger Monat',
     igx_calendar_next_month: 'Nächster Monat',
     igx_calendar_select_month: 'Wähle Monat',
@@ -30,5 +16,9 @@ export interface ICalendarResourceStrings {
     igx_calendar_singular_multi_selection: 'Mehrfachauswahl-Kalender ',
     igx_calendar_singular_range_selection: 'Datumsperiodenauswahl-Kalender',
     igx_calendar_singular_single_selection: 'Kalender',
-   };
-   
+};
+
+/**
+ * German resource strings for IgxCalendar
+ */
+export const CalendarResourceStringsDE = CalendarResourceStringsDE_ as ExpandRequire<ICalendarResourceStrings>;
