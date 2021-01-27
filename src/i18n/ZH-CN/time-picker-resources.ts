d40e82ccbab38a804ef8ff558f19b90f92fef47b
Rumyana Andriova <54146583+randriova@users.noreply.github.com>
2021-01-27 09:13:50 +0200

    Updating localization.

--- src/i18n/ZH-CN/time-picker-resources.ts
+++ src/i18n/ZH-CN/time-picker-resources.ts
@@ -2,8 +2,8 @@ import { ITimePickerResourceStrings } from 'igniteui-angular';
 
 // exported below as re-cast to create declaration type with expanded properties
 const TimePickerResourceStringsZHCN_: ExpandRequire<ITimePickerResourceStrings> = {
-    igx_time_picker_ok: 'OK',
-    igx_time_picker_cancel: 'Cancel'
+    igx_time_picker_ok: '确定',
+    igx_time_picker_cancel: '取消'
 };
 
 
