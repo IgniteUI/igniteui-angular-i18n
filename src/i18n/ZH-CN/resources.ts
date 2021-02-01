75df1248ced3c447f38dcb26998bab983badf623
iganchev <iganchev@infragistics.com>
2021-02-01 11:34:38 +0200

    Fix formating

--- src/i18n/ZH-CN/resources.ts
+++ src/i18n/ZH-CN/resources.ts
@@ -1,10 +1,10 @@
 import { CalendarResourceStringsZHCN } from './calendar-resources';
-import { GridResourceStringsZHCN} from './grid-resources';
-import { PaginatorResourceStringsZHCN} from './paginator-resources';
-import { TimePickerResourceStringsZHCN} from './time-picker-resources';
-import { DateRangePickerResourceStringsZHCN} from './date-range-picker-resources';
-import { CarouselResourceStringsZHCN} from './carousel-resources';
-import { ListResourceStringsZHCN} from './list-resources';
+import { GridResourceStringsZHCN } from './grid-resources';
+import { PaginatorResourceStringsZHCN } from './paginator-resources';
+import { TimePickerResourceStringsZHCN } from './time-picker-resources';
+import { DateRangePickerResourceStringsZHCN } from './date-range-picker-resources';
+import { CarouselResourceStringsZHCN } from './carousel-resources';
+import { ListResourceStringsZHCN } from './list-resources';
 
 /**
  * Chinese (PRC) zh-cn resource strings for all components
