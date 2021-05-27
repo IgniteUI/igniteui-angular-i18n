6f0a1f6216f785b3852cedf808916045f372aee4
iganchev <iganchev@infragistics.com>
2021-05-27 12:53:56 +0300

    Add a second i18n batch EN templates

---
+++ src/i18n/HU/list-resources.ts
@@ -0,0 +1,12 @@
+import { IListResourceStrings } from 'igniteui-angular';
+
+// exported below as re-cast to create declaration type with expanded properties
+const ListResourceStringsHU_: ExpandRequire<IListResourceStrings> = {
+    igx_list_no_items: 'There are no items in the list.',
+    igx_list_loading: 'Loading data from the server...'
+};
+
+/**
+ * Hungarian resource strings for IgxList
+ */
+export const ListResourceStringsHU = ListResourceStringsHU_ as ExpandRequire<IListResourceStrings>;
---
+++ src/i18n/PL/paginator-resources.ts
@@ -0,0 +1,16 @@
+import { IPaginatorResourceStrings } from 'igniteui-angular';
+
+// exported below as re-cast to create declaration type with expanded properties
+const PaginatorResourceStringsPL_: ExpandRequire<IPaginatorResourceStrings> = {
+    igx_paginator_label: 'Items per page',
+    igx_paginator_pager_text: 'of',
+    igx_paginator_first_page_button_text: 'Go to first page',
+    igx_paginator_previous_page_button_text: 'Previous page',
+    igx_paginator_last_page_button_text: 'Go to last page',
+    igx_paginator_next_page_button_text: 'Next page',
+};
+
+/**
+ * Polish resource strings for IgxPaginator
+ */
+export const PaginatorResourceStringsPL = PaginatorResourceStringsPL_ as ExpandRequire<IPaginatorResourceStrings>;
