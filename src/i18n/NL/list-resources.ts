b0b6404aa4da82aed3ba2478aab5c1afcd3a5984
iganchev <iganchev@infragistics.com>
2021-05-27 12:53:56 +0300

    Add a second i18n batch EN templates

---
+++ src/i18n/NL/list-resources.ts
@@ -0,0 +1,12 @@
+import { IListResourceStrings } from 'igniteui-angular';
+
+// exported below as re-cast to create declaration type with expanded properties
+const ListResourceStringsNL_: ExpandRequire<IListResourceStrings> = {
+    igx_list_no_items: 'There are no items in the list.',
+    igx_list_loading: 'Loading data from the server...'
+};
+
+/**
+ * Dutch resource strings for IgxList
+ */
+export const ListResourceStringsNL = ListResourceStringsNL_ as ExpandRequire<IListResourceStrings>;
