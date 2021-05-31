008f16d59e78d23123d7f1077fe9e6095a891758
Rumyana Andriova <54146583+randriova@users.noreply.github.com>
2021-05-31 13:20:06 +0300

    Small fix in RO.

--- src/i18n/RO/grid-resources.ts
+++ src/i18n/RO/grid-resources.ts
@@ -109,7 +109,7 @@ const GridResourceStringsRO_: ExpandRequire<IGridResourceStrings> = {
     igx_grid_actions_add_child_label: 'Adaugă copil',
     igx_grid_actions_delete_label: 'Șterge',
     igx_grid_actions_pin_label: 'Fixare',
-    igx_grid_actions_unpin_label: 'Anulați fixare',
+    igx_grid_actions_unpin_label: 'Anulați fixarea',
     igx_grid_actions_jumpUp_label: 'Sari sus',
     igx_grid_actions_jumpDown_label: 'Sari jos'
 };
