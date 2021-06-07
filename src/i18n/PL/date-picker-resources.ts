2c9c3f864b7c5b55133063727bf7987a6b951dbf
Rumy Andriova <54146583+randriova@users.noreply.github.com>
2021-06-07 16:35:37 +0300

    Updating Date Picker localization

--- src/i18n/PL/date-picker-resources.ts
+++ src/i18n/PL/date-picker-resources.ts
@@ -2,8 +2,8 @@ import { IDatePickerResourceStrings } from 'igniteui-angular';
 
 // exported below as re-cast to create declaration type with expanded properties
 const DatePickerResourceStringsPL_: ExpandRequire<IDatePickerResourceStrings> = {
-    igx_date_picker_change_date: 'Change Date',
-    igx_date_picker_choose_date: 'Choose Date'
+    igx_date_picker_change_date: 'Zmień datę',
+    igx_date_picker_choose_date: 'Wybierz datę'
 };
 
 /**
--- src/i18n/TR/date-picker-resources.ts
+++ src/i18n/TR/date-picker-resources.ts
@@ -2,8 +2,8 @@ import { IDatePickerResourceStrings } from 'igniteui-angular';
 
 // exported below as re-cast to create declaration type with expanded properties
 const DatePickerResourceStringsTR_: ExpandRequire<IDatePickerResourceStrings> = {
-    igx_date_picker_change_date: 'Change Date',
-    igx_date_picker_choose_date: 'Choose Date'
+    igx_date_picker_change_date: 'Tarihi değiştir',
+    igx_date_picker_choose_date: 'Tarih seçin'
 };
 
 /**
